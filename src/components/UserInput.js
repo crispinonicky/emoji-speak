3fee59eccaae8528935ccefebf8d585620ccc300
Nicholas Crispino <crispinonicky@gmail.com>
2019-03-28 18:46:06 -0400

    rough draft complete :)

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,43 +1,42 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React, { Component } from "react";
+import "../App.css";
 
 class UserInput extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      userString: '',
-    }
+      userString: ""
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
   }
 
   handleChange(event) {
-
-    function getRandomEmoji(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
+    // function getRandomEmoji(max) {
+    //   return Math.floor(Math.random() * Math.floor(max));
+    // }
 
     // get random emoji: emojiArray[getRandomEmoji(emojiArray.length)]
 
-    let emojiArray = ['😀', '😂', '🤩']
-    let emojiString = event.target.value
-    let testArray = []
+    // let emojiArray = ["😀", "😂", "🤩"];
+    let emojiString = event.target.value;
+    let testArray = [];
 
-    for (var i = 0; i < emojiString.length; i++) {
-      if (emojiString[i] === ' '){
-        console.log("Found a space!")
-        testArray.push(emojiArray[getRandomEmoji(emojiArray.length)])
-    } else {
-      testArray.push(emojiString[i])
-    }
-  }
+    // for (var i = 0; i < emojiString.length; i++) {
+    //   if (emojiString[i] === " ") {
+    //     console.log("Found a space!");
+    //     testArray.push(emojiArray[getRandomEmoji(emojiArray.length)]);
+    //   } else {
+    //     testArray.push(emojiString[i]);
+    //   }
+    // }
 
-  testArray = testArray.toString()
+    testArray = testArray.toString();
 
-  console.log(testArray)
+    
+
+    console.log(testArray);
 
     this.setState({
       userString: emojiString
@@ -45,77 +44,57 @@ class UserInput extends Component {
   }
 
   handleSubmit(event) {
-
     function getRandomEmoji(max) {
       return Math.floor(Math.random() * Math.floor(max));
     }
 
-    let emojiArray = ['😀', '😂', '🤩']
-    let emojiString = this.state.userString
-    let testArray = []
+    let emojiArray = ["😂", "🤩", "😎", "😫", "😬", "😱", "😵",
+  "👌", "🖕", "🔥", "💦", "🍆", "🍆", "🍑"
+  ];
+    let emojiString = this.state.userString;
+    let testArray = [];
 
     for (var i = 0; i < emojiString.length; i++) {
-      if (emojiString[i] === ' '){
-        console.log("Found a space!")
-        testArray.push(emojiArray[getRandomEmoji(emojiArray.length)])
-    } else {
-      testArray.push(emojiString[i])
+      if (emojiString[i] === " ") {
+        console.log("Found a space!");
+        testArray.push(" " + emojiArray[getRandomEmoji(emojiArray.length)] + " ");
+      } else {
+        testArray.push(emojiString[i]);
+      }
     }
-  }
 
-  testArray = testArray.toString()
+    testArray = testArray.join("");
 
-  console.log(testArray)
+    console.log(testArray);
 
     document.write(testArray);
     event.preventDefault();
   }
 
-  
-
   render() {
-
-    console.log(this.state.userString)
-
+    console.log(this.state.userString);
 
     return (
       <div className="user-input boxsizingBorder">
-
-      <form onSubmit = {this.handleSubmit}>
-        <label>
-          Got anything to say? 
-          <br/><br/>
-          <textarea 
-          rows = "20"
-          cols = "50"
-          type = "text" 
-          value = {this.state.userString} 
-          onChange = {this.handleChange}
-          />
-          <br/> <br/>
-        </label>
-        <input 
-        type = "submit"
-        value = "Submit"
-        />
-
-      
-      </form>      
-
-
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Got anything to say?
+            <br />
+            <br />
+            <textarea
+              rows="20"
+              cols="50"
+              type="text"
+              value={this.state.userString}
+              onChange={this.handleChange}
+            />
+            <br /> <br />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
-
-      
     );
-
-    
-
   }
-
-  
-  
 }
-
-
 
 export default UserInput;
